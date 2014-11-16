6dfa6992a8665035b98a42bc4fcb46afedf2da67
NimitzDEV <nimitzdevteam@gmail.com>
2014-11-16 19:40:30 +0800

    Bump (2.7.1.1)

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 方法是按如下所示使用“*”:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.7.0.2")> 
-<Assembly: AssemblyFileVersion("2.7.0.2")> 
+<Assembly: AssemblyVersion("2.7.1.1")> 
+<Assembly: AssemblyFileVersion("2.7.1.1")> 
